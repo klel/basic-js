a99a3d8d523289be3e2e9d859a6e74497eac4c19
devEl <klimelm@gmail.com>
2022-03-01 21:46:33 +0300

    feat: 3 tasks solved

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const map1 = s1.split('')
+    .reduce((acc, curr) => {
+      acc[curr] ? acc[curr] += 1 : acc[curr] = 1;
+      return acc;
+    }, {});
+
+  const map2 = s2.split('')
+    .reduce((acc, curr) => {
+      acc[curr] ? acc[curr] += 1 : acc[curr] = 1;
+      return acc;
+    }, {});
+
+  const result = Object.entries(map1).reduce((acc, [prop, count]) => {
+    if (prop in map2) {
+      return acc += Math.max(count, map2[prop]) - Math.abs(count - map2[prop]);
+    }
+    return acc;
+  }, 0);
+
+  return result;
+
+
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((acc, curr) => acc.concat(...curr), [])
+    .filter(e => e === '^^')
+    .length;
 }
 
 module.exports = {
